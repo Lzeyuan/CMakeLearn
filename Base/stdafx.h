8f91c5497682b65a0ab66d267e45e4c908f08614
Leza <1046527915@qq.com>
2024-01-23 12:54:31 +0800

    修改项目结构，把阶段性总结作为case。

---
+++ Base/stdafx.h
@@ -0,0 +1,23 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <functional>
+#include <memory>
+#include <cstdio>
+#include <optional>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#endif //STDAFX_H
